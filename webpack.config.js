c6d32a55ae36e2967ae7821d49f593f7e62febed
Shagun Madhikarmi <madhikarma@gmail.com>
2018-02-24 12:57:02 +0000

    Auto Prettier formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,28 @@
 // entry -> output
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/app.js',
+  entry: "./src/app.js",
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
   },
   module: {
     rules: [
-    {
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, 
-    {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, "public")
   }
 };
 
