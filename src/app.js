a7acd2fee39851cb4557416dc8dec3ba4c95aee3
Shagun Madhikarmi <madhikarma@gmail.com>
2018-02-24 13:50:28 +0000

    - Adds basic HelloWorld React component with stylesheet. - Adds normalize lib to remove default browser styling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import HelloWorld from "./components/HelloWorld.js";
+import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>Hello World</p>, document.getElementById("app"));
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div className="helloWorldContainer">
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
